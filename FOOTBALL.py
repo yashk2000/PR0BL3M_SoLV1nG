95f30ca3356c01f080e8ae8927f02d0592766b55
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-10-27 01:30:07 +0530

    Update and rename FOOTBALL.py to FOOTBALL.java

--- FOOTBALL.py
+++
@@ -1,9 +0,0 @@
-str=raw_input("enter arrangment of players:")
-count=0
-for i in range (len(str)-1):
-    if(str[i]==str[i+1]):
-       count=count+1
-if(count>=7):
-   print "dangerous"
-else:
-   print "not dangerous"
