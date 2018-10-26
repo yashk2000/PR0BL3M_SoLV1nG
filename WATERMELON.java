1c5e49462304f695633226606db648e7561ad9b4
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-10-27 01:23:47 +0530

    Update and rename WATERMELON.c to WATERMELON.java

---
+++ WATERMELON.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n==2)
+    System.out.println("NO");
+    else if(n%2==0)
+    System.out.println("YES");
+    else
+    System.out.println("NO");
+  }
+}
