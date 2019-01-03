ff19db913d2bf2b775b1293755d3543a945daaf4
Yash Khare <yashsja@gmail.com>
2019-01-03 13:01:50 +0530

    Add files via upload

---
+++ COMAS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class COMAS{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int  t=sc.nextInt();
+      for(int i=0;i<t;++i){
+        int m=sc.nextInt();
+        System.out.println("? 1 2 3 4 5");
+        System.out.flush();
+        int tt=sc.nextInt();
+        int ttt=sc.nextInt();
+        System.out.println("! "+m);System.out.flush();
+      }
+    }
+}
