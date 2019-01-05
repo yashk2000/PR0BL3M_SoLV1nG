ba806b4b3a55d2fdc9bdfb0b9e4cebe215f1a274
Yash Khare <yashsja@gmail.com>
2019-01-05 20:40:49 +0530

    Add files via upload

---
+++ CHFIDEAL.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class CHFIDEAL{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=3;
+    System.out.println(t);
+    System.out.flush();
+    int a=sc.nextInt();
+    if(a==2){
+    System.out.println("1");System.out.flush();}
+    else{
+    System.out.println("2");System.out.flush();}
+  }
+}
